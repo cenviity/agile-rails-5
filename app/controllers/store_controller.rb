6a282ddab14140384a7713e7078d5d169cf2b79b
Vincent Yim <cenviity@gmail.com>
2017-08-20 14:01:04 +0000

    Add variable to session to record how many times index has been accessed

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,9 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    
+    session[:counter] ||= 1
+    session[:counter] += 1
+    @index_views = session[:counter]
   end
 end

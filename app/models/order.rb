fa530108e6c15855bcebafbe9269d2165a54d837
Vincent Yim <cenviity@gmail.com>
2017-08-21 14:33:35 +0000

    Add has_many for line items

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,8 @@ class Order < ApplicationRecord
     "Purchase order" => 2
   }
   
+  has_many :line_items, dependent: :destroy
+  
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: pay_types.keys
 end

409c4cc43777fda477e740ff30265ded944134ce
Vincent Yim <cenviity@gmail.com>
2017-08-22 16:00:14 +0000

    Establish relationship between Order and PaymentType

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,5 @@
 class Order < ApplicationRecord
-  enum pay_type: {
-    "Check"          => 0,
-    "Credit card"    => 1,
-    "Purchase order" => 2
-  }
-  
+  belongs_to :payment_type
   has_many :line_items, dependent: :destroy
   
   validates :name, :address, :email, presence: true

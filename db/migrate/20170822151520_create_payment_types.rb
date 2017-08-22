20c577799cf41427d1be16b59ad2fa46f6505500
Vincent Yim <cenviity@gmail.com>
2017-08-22 15:20:09 +0000

    Add default payment types

--- db/migrate/20170822151520_create_payment_types.rb
+++ db/migrate/20170822151520_create_payment_types.rb
@@ -5,5 +5,10 @@ class CreatePaymentTypes < ActiveRecord::Migration[5.0]
 
       t.timestamps
     end
+    
+    payment_types = ['Check', 'Credit card', 'Purchase order']
+    payment_types.each do |payment_type|
+      PaymentType.create(name: payment_type)
+    end
   end
 end

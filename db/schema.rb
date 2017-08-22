feb6cacd17a4586075fedc247f536b36d335d8fd
Vincent Yim <cenviity@gmail.com>
2017-08-22 15:20:26 +0000

    Apply migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170820162128) do
+ActiveRecord::Schema.define(version: 20170822151520) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -35,6 +35,12 @@ ActiveRecord::Schema.define(version: 20170820162128) do
     t.text     "address"
     t.string   "email"
     t.integer  "pay_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "payment_types", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

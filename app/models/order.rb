e5793f55b0241cff24bed4321a12cff8e8bd4105
Vincent Yim <cenviity@gmail.com>
2017-08-22 16:10:12 +0000

    Replace references to pay_type with payment_type

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
   has_many :line_items, dependent: :destroy
   
   validates :name, :address, :email, presence: true
-  validates :pay_type, inclusion: pay_types.keys
+  validates :payment_type, inclusion: { in: PaymentType.all }
   
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|

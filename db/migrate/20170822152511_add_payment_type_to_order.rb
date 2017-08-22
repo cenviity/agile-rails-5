0aedf39a0c34776b699e268f38aecdc918a01131
Vincent Yim <cenviity@gmail.com>
2017-08-22 15:33:18 +0000

    Edit migration to replace old pay_type column with new payment_type

--- db/migrate/20170822152511_add_payment_type_to_order.rb
+++ db/migrate/20170822152511_add_payment_type_to_order.rb
@@ -1,5 +1,33 @@
 class AddPaymentTypeToOrder < ActiveRecord::Migration[5.0]
-  def change
+  def up
     add_reference :orders, :payment_type, foreign_key: true
+    
+    Order.all.each do |order|
+      order.update(payment_type_id:
+        case order.pay_type
+        when 'Check'          then 1
+        when 'Credit card'    then 2
+        when 'Purchase order' then 3
+        end
+      )
+    end
+    
+    remove_column :orders, :pay_type
+  end
+  
+  def down
+    add_column :orders, :pay_type, :integer
+    
+    Order.all.each do |order|
+      order.update(pay_type:
+        case order.payment_type
+        when 1 then 'Check'
+        when 2 then 'Credit card'
+        when 3 then 'Purchase order'
+        end
+      )
+    end
+    
+    remove_reference :orders, :payment_type
   end
 end

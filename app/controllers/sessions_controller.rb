94be8eecb740128733c298f665e880bba7e410a3
Vincent Yim <cenviity@gmail.com>
2017-08-23 12:31:23 +0000

    Extract logic for getting user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,16 @@
 class SessionsController < ApplicationController
   skip_before_action :authorise
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_login
 
   def new
   end
 
   def create
-    user = User.find_by(name: params[:name]) ||
-      User.create(name: params[:name], password: params[:password])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to admin_url
     else
-      redirect_to login_url, alert: "Invalid user/password combination"
+      invalid_login
     end
   end
 
@@ -19,4 +18,18 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to store_index_url, notice: "Logged out"
   end
+  
+  private
+  
+    def user
+      if User.count.zero?
+        User.create!(name: params[:name], password: params[:password])
+      else
+        User.find_by(name: params[:name])
+      end
+    end
+    
+    def invalid_login
+      redirect_to login_url, alert: "Invalid user/password combination"
+    end
 end

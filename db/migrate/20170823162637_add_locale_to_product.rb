1485dbfe70fe9130ce93cb98b4cfeeb27b977e3f
Vincent Yim <cenviity@gmail.com>
2017-08-23 16:29:54 +0000

    Apply migration; set locale of existing products to 'en'

--- db/migrate/20170823162637_add_locale_to_product.rb
+++ db/migrate/20170823162637_add_locale_to_product.rb
@@ -1,5 +1,9 @@
 class AddLocaleToProduct < ActiveRecord::Migration[5.0]
   def change
     add_column :products, :locale, :string
+    
+    Product.all.each do |product|
+      product.update(locale: 'en')
+    end
   end
 end

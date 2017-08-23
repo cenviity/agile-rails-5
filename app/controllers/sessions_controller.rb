55e3c6f2995e72f4214dc267b562c35f84a5d7bd
Vincent Yim <cenviity@gmail.com>
2017-08-23 09:59:32 +0000

    Skip authorise callback before certain actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :authorise
+
   def new
   end
 
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,5 @@
 class StoreController < ApplicationController
+  skip_before_action :authorise
   include CurrentCart
   before_action :set_cart
   

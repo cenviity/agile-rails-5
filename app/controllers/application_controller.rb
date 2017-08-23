cf6a8bb03465e57e9674809c5f00bd0e88527cf6
Vincent Yim <cenviity@gmail.com>
2017-08-23 13:32:42 +0000

    Respond to request formats other than HTML

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,14 @@ class ApplicationController < ActionController::Base
   
     def authorise
       unless User.find_by(id: session[:user_id])
-        redirect_to login_url, notice: "Please log in"
+        if request.format == Mime[:html]
+          redirect_to login_url, notice: "Please log in"
+        else
+          authenticate_or_request_with_http_basic do |name, password|
+            user = User.find_by(name: name)
+            user&.authenticate(password)
+          end
+        end
       end
     end
 end

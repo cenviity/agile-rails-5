076d4046bbf80cf710ee0e4b684d320a78964b58
Vincent Yim <cenviity@gmail.com>
2017-08-23 13:52:28 +0000

    Add tests for requiring authentication to fetch and update data

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -23,5 +23,16 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     delete logout_url
     assert_redirected_to store_index_url
   end
-
+  
+  test "should require authentication to fetch data" do
+    delete logout_url
+    get orders_url
+    assert_redirected_to login_url
+  end
+  
+  test "should require authentication to update data" do
+    delete logout_url
+    patch order_url(orders(:one)), params: { email: "new@email.com" }
+    assert_redirected_to login_url
+  end
 end
